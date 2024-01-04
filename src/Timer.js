91870d09444478bb848ffba2eb76e9afedec8542
minimomo14 <93943633+minimomo14@users.noreply.github.com>
2024-01-04 18:45:19 -0500

    added Timer.js

---
+++ src/Timer.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+
+const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let timer;
+
+    if (isRunning) {
+      timer = setInterval(() => {
+        setSeconds((prevSeconds) => prevSeconds + 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(timer);
+  }, [isRunning]);
+
+  const startTimer = () => {
+    setIsRunning(true);
+  };
+
+  const stopTimer = () => {
+    setIsRunning(false);
+  };
+
+  const resetTimer = () => {
+    setSeconds(0);
+    setIsRunning(false);
+  };
+
+  return (
+    <div>
+      <h1>Timer App</h1>
+      <div>
+        <p>Seconds: {seconds}</p>
+        <button onClick={startTimer}>Start</button>
+        <button onClick={stopTimer}>Stop</button>
+        <button onClick={resetTimer}>Reset</button>
+      </div>
+    </div>
+  );
+};
+
+
+
+export default Timer;
